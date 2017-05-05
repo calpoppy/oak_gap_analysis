51041ec58f54a6e2ea224f4a86eb685b9a6ad7e1
Emily Beckman <ebeckman@mortonarb.org>
2017-05-05 11:59:08 -0500

    Comment cleanup

--- subset_exsitu_data.r
+++ subset_exsitu_data.r
@@ -1,7 +1,9 @@
-# import packages
+## IMPORT PACKAGES:
+
 library(plyr); library(shiny); library(stringr); library(dplyr); library(data.table)
 
-# FUNCTIONS
+## FUNCTIONS :
+
 # subset data and (optionally!) write a CSV
 gen_subset <- function(orig_data, action, export_name){
   selected_rows <- (action)
@@ -13,8 +15,9 @@ gen_subset <- function(orig_data, action, export_name){
       return(data.frame(new))
     }
 }
+
 # count records by unique values in given column, return in ascending order of frequency
-# and (optionally!) write a CSV
+  # and (optionally!) write a CSV
 count_records <- function(dataset, col, export_name){
   new <- count(dataset, col)
   new <- new[with(new, order(freq)),]
@@ -25,8 +28,10 @@ count_records <- function(dataset, col, export_name){
       return(data.frame(new))
     }
 }
-#  ---- not currently used!
+
+#  ------- not currently used! ---------
 # use when you do not want to keep non-shared columns
+# heres how it works:
   # 1. Specify the input dataframes
   # 2. Calculate which dataframe has the greatest number of columns
   # 3. Identify which columns in the smaller dataframe match the columns in the larger dataframe
@@ -46,66 +51,64 @@ rbind.match.columns <- function(input1, input2) {
     return(rbind(input1[, column.names], input2[, column.names]))
 }
 
+# BEGIN : Create Data Subsets
 
-# Create Data Subsets
-# *** read in new full dataset from Dataiku, with species column added ***
+ # 1) Read in datasets from ***Dataiku***, with species column added
+   # The analysis and creation of the "species" column is housed in Dataiku [Data Science Studio] right now,
+   # but should probably be converted to R at some point!!
+   # The initial input for Dataiku is the file created through "compile_exsitu_data.r"
+     # species column has been added to this dataset:
 all_data_scrub1 <- read.csv('./exsitu_data/exsitu_all_data_10_prepared.csv',
     as.is=TRUE, sep=',',row.names=NULL, strip.white = TRUE);
-# *** read in Dataiku file with species standardized - rows REMOVED for hybrids, unknowns, etc. ***
+     # species column has been standardized, and rows REMOVED for hybrids, unknowns, etc.
 all_data_spscrub <- read.csv('./exsitu_data/exsitu_all_data_10_prepared_species.csv',
     as.is=TRUE, sep=',',row.names=NULL, strip.white = TRUE);
-# print number of records for each insitution, before and after Dataiku species cleaning
-# count(all_data_scrub1$inst_short) -- not working???
-# count(all_data_spscrub$inst_short)
+     # print number of records for each insitution, before and after Dataiku species cleaning
+#count(all_data_scrub1$inst_short) -- not working now??? was working before and I don't think I changed anything...
+#count(all_data_spscrub$inst_short)
 unique(all_data_scrub1$inst_short)
 
-
-# Join to (merge with) Institution Data Table
+# 2) Join to [merge with] Institution Data Table
+  # ----------- this is not quite right! for the institutions with the same "inst_short" name,
+  # but unique inst_short2 [like in Quercus PCN], duplicates of every accession are made for every "inst_short2"
 #inst_data <- read.csv('./exsitu_data/InstitutionDataTable.csv',
     #as.is=TRUE, sep=',',row.names=NULL, strip.white = TRUE);
 #all_data_spscrub <- join(all_data_spscrub, inst_data, by = "inst_short", type = "full")
 
-# 1. Species Subsets:
-# read in species of interest list(s)
+# 3) Subset by species name
+  # read in species of interest list(s)
 sp_of_concern <- read.csv('./sp_occ/species_lists/sp_of_concern.csv',
     as.is=TRUE, sep=',',row.names=NULL, strip.white = TRUE);
 sp_us_all <- read.csv('./sp_occ/species_lists/sp_us_all.csv',
     as.is=TRUE, sep=',',row.names=NULL, strip.white = TRUE);
-# call subset function
+  # call subset function
+    # only accessions for US oaks will remain :)
 us_all <- gen_subset(all_data_spscrub, (all_data_spscrub$species %in% sp_us_all$species), "./exsitu_data/us_subsets/all_data_US.csv")
 nrow(us_all)
 unique(us_all$inst_short)
-  #count(us_all$inst_short)
-  #count(us_all$species)
+          #count(us_all$inst_short) --- not working.
+          #count(us_all$species)
+    # only accessions for US oaks of concern will remain :)
 us_concern <- gen_subset(all_data_spscrub, (all_data_spscrub$species %in% sp_of_concern$species), "./exsitu_data/concern_subsets/all_data_USconcern.csv")
 nrow(us_concern)
 unique(us_concern$inst_short)
-  #count(us_concern$inst_short)
-  #count(us_all$species)
-  #str(us_concern)
-        # NOT WORKING -- order us_concern from broad to fine locality info available
-#us_concern <- arrange(us_concern, desc(dd_conv_needed), desc(latlong_geopoint))
-#us_concern <- us_concern[with(us_concern, order(source,  latlong_geopoint)),]
-#us_concern <- us_concern[with(us_concern, order(desc(dd_conv_needed), desc(latlong_geopoint))),]
-#desc(source), desc(other_region), desc(us_state), desc(maj_region),
-  #desc(min_region), desc(us_county), desc(locality), desc(dd_conv_needed), latlong_geopoint)
+          #count(us_concern$inst_short) --- not working.
+          #count(us_all$species)
 
-## continue with species of concern subset only--
+## continue with species of concern subset only --->
 
-# 2. Lat/Long Subsets:
-# add columns for determination of lat-long
+# 4) Subset and clean up lat and long coordinates
+  # add columns for determination of lat-long
 us_concern$gps_det <- as.character(NA)
 us_concern$gps_prec_place <- as.character(NA)
 us_concern$gps_prec_value_km2 <- as.numeric(NA)
 us_concern$lat <- as.numeric(us_concern$orig_lat)
 us_concern$long <- as.numeric(us_concern$orig_long)
-
-# records with valid lat/long values
+  # records with valid lat/long values
 conc_has_latlong <- gen_subset(us_concern,!is.na(us_concern$lat))
 conc_has_latlong$gps_det <- "G"
 nrow(conc_has_latlong)
-
-# switch lat and long values if lat is greater than 50 or less than 0
+  # switch lat and long values if lat is greater than 50 or less than 0
 for (r in seq_along(conc_has_latlong$lat)){
   temp <- conc_has_latlong$lat[r]
   if (temp > 50 || temp < 0){
@@ -113,44 +116,40 @@ for (r in seq_along(conc_has_latlong$lat)){
     conc_has_latlong$long[r] <- temp
   }
 }
-# add a minus sign if the longitude is positive; write csv
+  # add a minus sign if the longitude is positive; write csv
 for (r in seq_along(conc_has_latlong$long)){
   if (conc_has_latlong$long[r] > 0){
       conc_has_latlong$long[r] <- -1 * conc_has_latlong$long[r]
   }
 }
 write.csv(conc_has_latlong, file = "./exsitu_data/concern_subsets/has_latlong.csv")
-# records needing dms to dd conversion
+  # records needing dms to dd conversion
 conc_conv_ll <- gen_subset(us_concern, (us_concern$dd_conv_needed == "x"))
 conc_conv_ll$gps_det <- "M"
 write.csv(conc_conv_ll, file = "./exsitu_data/concern_subsets/needs_convert.csv")
 nrow(conc_conv_ll)
-
-# remove rows already exported through 'has_latlong'
+  # remove rows already exported through 'has_latlong'
 us_concern_loc <- gen_subset(us_concern, (us_concern$latlong_geopoint == ""))
 nrow(us_concern_loc)
-# remove rows already exported through 'needs_convert'
+  # remove rows already exported through 'needs_convert'
 us_concern_loc2 <- gen_subset(us_concern_loc, (us_concern_loc$dd_conv_needed == ""))
 nrow(us_concern_loc2)
 write.csv(us_concern_loc2, file = "./exsitu_data/concern_subsets/us_concern_loc2.csv")
-# remove rows where g_prov is 1 (source column contains gdn, n., farm, nursery, inc., garden, bg, arboretum, or arb)
-#us_concern_loc3 <- gen_subset(us_concern_loc2, is.na(us_concern_loc2$g_prov))
-#nrow(us_concern_loc3)
-#write.csv(us_concern_loc3, file = "./exsitu_data/concern_subsets/us_concern_loc3.csv")
-# remove rows with no locality info
+
+# 5) Subset by locality info
+  # remove rows with no locality info
 us_concern_loc3 <- gen_subset(us_concern_loc2, !is.na(us_concern_loc2$all_locality))
 nrow(us_concern_loc3)
 write.csv(us_concern_loc3, file = "./exsitu_data/concern_subsets/us_concern_loc3.csv")
-#remove rows with duplicate localities and sum no_alive for deleted rows
-us_concern_unq <- aggregate(no_alive ~ all_locality+us_county+us_state+inst_short+inst_short2+species+specific, data = us_concern_loc3, FUN = sum)
+  # remove rows with duplicate locality descriptions, and sum no_alive [number of individuals alive] for these deleted rows
+us_concern_unq <- aggregate(no_alive ~ all_locality+us_county+us_state+inst_short+inst_short2+species+specific, 
+   data = us_concern_loc3, FUN = sum)
 nrow(us_concern_unq)
 str(us_concern_unq)
-# remove rows with no locality info (this includes 'source' which only sometimes holds locality data)
-#us_concern_loc <- gen_subset(us_concern_unq, (us_concern_unq$all_locality != ""))
-#nrow(us_concern_loc)
+  # write a final CSV
 write.csv(us_concern_unq, file = "./exsitu_data/concern_subsets/us_concern_unq_locality.csv")
 
-########### Stats ############ ----all suddenly not working???? -- "no applicable method for 'group_by_' applied to an object of class "character""
+########### STATS ############ ----all suddenly not working??? -- "no applicable method for 'group_by_' applied to an object of class "character""
 # all US species
 acc_per_inst_us <- count(us_all, inst_short)
 write.csv(acc_per_inst_us, file = "./exsitu_data/us_subsets/acc_per_inst_us.csv")
@@ -176,14 +175,7 @@ write.csv(acc_per_sp_per_inst, file = "./exsitu_data/concern_subsets/acc_per_sp_
 
 ########################## CURRENT END ##############################
 
-
-
-
-      # indiv_per_sp <-
-      # sp_vs_cultivar <-
-
-
-
+## Just playing with ShinyApp and summary tables :
 
 # Create a nice table of record numbers
   # create data frame of values to display
@@ -195,7 +187,6 @@ no_records_v <- c(nrow(all_data_spscrub), nrow(us_all), nrow(us_concern),
 table <- (data.frame("Values Subset" = gen_subset_v,
   "Species Subset" = sp_subset_v,
   "Num Records" = no_records_v))
-
 
 # create app to display summary table
 
